68467c33249b91701838d3c6bb4f56d7ff0a9988
oonaka.k@gmail.com <oonaka.k@gmail.com>
2018-12-04 21:49:47 +0900

    初期構造追加

---
+++ iOS/Turi/Turi/view/FavoViewController.swift
@@ -0,0 +1,35 @@
+//
+//  FavoViewController.swift
+//  Turi
+//
+//  Created by oonaka on 2018/12/04.
+//  Copyright © 2018 hasi.123hasi.123. All rights reserved.
+//
+
+import UIKit
+
+class FavoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ iOS/Turi/Turi/view/customToolBar.swift
@@ -0,0 +1,17 @@
+//
+//  CustomToolBar.swift
+//  Turi
+//
+//  Created by oonaka on 2018/12/04.
+//  Copyright © 2018 hasi.123hasi.123. All rights reserved.
+//
+
+import UIKit
+
+class CustomToolBar: UIToolbar {
+
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+
+}
