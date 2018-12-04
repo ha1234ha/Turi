cc3e15d89d502b79b4e6ed10e34163ff2c3e3359
oonaka.k@gmail.com <oonaka.k@gmail.com>
2018-12-04 20:20:22 +0900

    新規作成

---
+++ iOS/Turi/Turi/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Turi
+//
+//  Created by oonaka on 2018/12/04.
+//  Copyright © 2018 hasi.123hasi.123. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
